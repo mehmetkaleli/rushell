c94ba53ba58f6af76f0cb9fb1ad84266c502c1d3
Mohammed Briman <mehmet.kaleli1496@gmail.com>
2025-05-11 19:18:29 +0300

    add echo command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn builtin(input: &str) {
+    if input == "exit" || input == "echo" || input == "type" {
+        println!("{} is a shell builtin", &input);
+    } else {
+        println!("{}: not found", &input);
+    }
+}
+
 fn repl(){
     loop {
         // read command
@@ -15,6 +23,7 @@ fn repl(){
         match input.trim() {
             "exit 0" => std::process::exit(0),
             input if input.starts_with("echo") => println!("{}", &input[5..]), 
+            input if input.starts_with("type") => builtin(&input[5..]),
             _ => println!("{}: command not found", input.trim())
         }
     }
