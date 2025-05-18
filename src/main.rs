87ce8d4dce692b525361127ceaaf2da6de3a9baf
Mohammed Briman <mehmet.kaleli1496@gmail.com>
2025-05-18 21:08:49 +0300

    fix format issue for echo command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::io::{self, Write};
 use std::env;
 use std::path::{PathBuf};
 
+const BUILTIN_COMMANDS: &[&str] = &["exit", "echo", "type"];
+
 fn is_builtin(input: &str) {
     match env::var("PATH") {
         Ok(path) => {
@@ -12,7 +14,7 @@ fn is_builtin(input: &str) {
                 let path = dir.join(input).to_string_lossy().trim().to_string();
                 let path = PathBuf::from(path);
                 if path.is_file(){
-                    println!("{} is {}", input, path.display());
+                    println!("{} is {}", input.trim(), path.display());
                     found = true;
                     break;
                 }
@@ -50,8 +52,15 @@ fn main() {
         // match command and handel args
         match command {
             command if command.starts_with("exit") => std::process::exit(0),
-            command if command.starts_with("echo") => println!("{:?}", &args.get(0).unwrap()), 
-            command if command.starts_with("type") => is_builtin(&input[5..]), // refactor to search PATH
+            command if command.starts_with("echo") => println!("{}", &args.join(" ").as_str()), 
+            command if command.starts_with("type") => {
+                let type_arg = args[0];
+                if BUILTIN_COMMANDS.contains(&type_arg){
+                    println!("{} is a shell builtin", type_arg);
+                } else {
+                    is_builtin(type_arg)
+                }
+            }, 
             _ => println!("{}: command not found", input.trim())
         }
     }
